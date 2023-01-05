3908de7a9ee44afe40f3968189dbd7dec44de3b0
najibmuqeem <najib.muqeem@gmail.com>
2023-01-04 20:01:43 -0500

    MVP complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { BsFillTelephoneFill } from "react-icons/bs";
-import { FaUserAlt } from "react-icons/fa";
+import { FaRecordVinyl, FaUserAlt } from "react-icons/fa";
+import { IoIosKeypad, IoIosSettings } from "react-icons/io";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-activity footer-selected">
+      <div className="footer-item footer-selected">
         <BsFillTelephoneFill />
       </div>
-      <div className="footer-user">
+      <div className="footer-item">
         <FaUserAlt />
+      </div>
+      <div className="footer-item">
+        <IoIosKeypad />
+      </div>
+      <div className="footer-item">
+        <IoIosSettings />
+      </div>
+      <div className="footer-item">
+        <FaRecordVinyl />
       </div>
     </div>
   );
